e17bbe63e5a6599621b145592acec551e00c0ca6
Utsav Shrestha <160091926+Utsav-345@users.noreply.github.com>
2025-01-18 19:08:20 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function Shift(){
+    //Button click active class
+    for(let i=0; i<sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className=currentBtn[0].className.replace('active-btn','');
+            this.className+=' active-btn';
+        })
+    }
+
+    //sections active class
+    allSections.addEventListener('click',(e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other btn
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+            const element=document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+    //toogle theme
+    const themeBtn=document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element=document.body;
+        element.classList.toggle('light-mode')
+    })
+
+
+}
+
+Shift();
